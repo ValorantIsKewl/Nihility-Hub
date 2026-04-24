062c6dda0f9eecfeb5ae22aba280b9a9a6a07f86
S. <valorantalt924@gmail.com>
2026-04-24 03:10:11 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 
 local id = game.GameId
-if id == 2414851778 then
+if id == 848145103 then
     -- Dungeon Quest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/DQ.lua"))()
 elseif id == 125503319883299 then
