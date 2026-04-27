8222f34ac6142f60d7a2e99e4298d66c54438c5c
S. <valorantalt924@gmail.com>
2026-04-27 11:40:43 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ local id = game.GameId
 if id == 848145103 then
     -- Dungeon Quest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/DQ.lua"))()
-elseif id == 125503319883299 then
+elseif id == 7093527744 then
     -- Arcane Conquest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/AC.lua"))()
 else
