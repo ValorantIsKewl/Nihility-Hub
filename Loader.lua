217428b45f2f2114dcfa4513baea61b4d2ac69cc
S. <valorantalt924@gmail.com>
2026-05-08 15:24:00 -0400

    Add support for Dungeon Heroes and Slime RNG

--- Loader.lua
+++ Loader.lua
@@ -7,6 +7,12 @@ if id == 848145103 then
 elseif id == 7093527744 then
     -- Arcane Conquest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/AC.lua"))()
+elseif id == 7546582051 then
+    -- Dungeon Heroes
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/DH.lua"))()
+elseif id == 9792947201 then
+    -- Slime RNG
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ValorantIsKewl/Nihility-Hub/refs/heads/main/Games/sRNG.lua"))()
 else
     warn("Game Not Supported!")
 end
